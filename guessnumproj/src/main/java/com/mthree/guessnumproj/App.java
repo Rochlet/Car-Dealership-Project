2f30cf9c10fb4a6e02437d2c8f7bdccdb64b962b
Armando Gonzalez <armangonzal@gmail.com>
2021-09-15 14:22:27 -0500

    Update App.java

--- guessnumproj/src/main/java/com/mthree/guessnumproj/App.java
+++ guessnumproj/src/main/java/com/mthree/guessnumproj/App.java
@@ -4,11 +4,20 @@
  * and open the template in the editor.
  */
 package com.mthree.guessnumproj;
-
+import com.mthree.guessnumproj.controller.*;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 /**
  *
  * @author ArmandoGonzalez
  */
 public class App {
-    
+    public static void main(String[] args) {
+
+        AnnotationConfigApplicationContext appContext = new AnnotationConfigApplicationContext();
+        appContext.scan("com.mthree.guessnumproj");
+        appContext.refresh();
+
+        Controller controller = appContext.getBean("Controller", Controller.class);
+        //controller.run();
+    }
 }
